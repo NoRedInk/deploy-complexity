6ba4eae6894953fe3b91227ea6f1ace7d3b091a3
Marica Odagaki <marica@noredink.com>
2020-05-04 16:04:15 -0800

    nixfmt

--- shell.nix
+++ shell.nix
@@ -1,16 +1,15 @@
 with import (builtins.fetchTarball rec {
   # grab a hash from here: https://nixos.org/channels/
   name = "nixpkgs-darwin-20.03pre214403.c23427de0d5";
-  url = "https://github.com/nixos/nixpkgs/archive/c23427de0d501009b9c6d77ff8dda3763c6eb1b4.tar.gz";
+  url =
+    "https://github.com/nixos/nixpkgs/archive/c23427de0d501009b9c6d77ff8dda3763c6eb1b4.tar.gz";
   # Hash obtained using `nix-prefetch-url --unpack <url>`
   sha256 = "0fgv4pyzn39y8ibskn37x9cabmg6gflisigr5l45bkplm06bss91";
-}) {};
+}) { };
 let
   expectVersion = version: pkg:
-    let
-      actual = lib.getVersion pkg;
-    in
-    assert (lib.assertMsg (builtins.toString actual == version) ''
+    let actual = lib.getVersion pkg;
+    in assert (lib.assertMsg (builtins.toString actual == version) ''
       Expecting version of ${pkg.name} to be ${version} but got ${actual};
       adjust the expected version or fetch/build the desired version of the package.
     '');
@@ -18,8 +17,7 @@ let
 
   rubyVersion = lib.fileContents ./.ruby-version;
   bundlerVersion = lib.fileContents ./.bundler-version;
-in
-stdenv.mkDerivation {
+in stdenv.mkDerivation {
   name = "deploy-complexity";
   buildInputs = [
     git
