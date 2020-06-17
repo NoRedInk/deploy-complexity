95809e387e4e782f422cc13b2b734d884af3c81b
Charles Comstock <dgtized@gmail.com>
2020-06-17 12:43:30 -0500

    re-indent heredoc per RUBOCOP

--- spec/lib/deploy_complexity/checklists_spec.rb
+++ spec/lib/deploy_complexity/checklists_spec.rb
@@ -110,24 +110,24 @@ describe Checklists do
     it_behaves_like "a checklist class"
 
     it 'matches on a diff insertion' do
-      patch = <<DIFF
-diff --git a/deploy-complexity.gemspec b/deploy-complexity.gemspec
-index 1f82fcb..b2e4ffb 100644
---- a/deploy-complexity.gemspec
-+++ b/deploy-complexity.gemspec
-@@ -26,10 +26,10 @@ Gem::Specification.new do |spec|
-   spec.add_development_dependency "bundler", "~> 2.1"
-   spec.add_development_dependency "rake", "~> 13.0"
+      patch = <<~DIFF
+        diff --git a/deploy-complexity.gemspec b/deploy-complexity.gemspec
+        index 1f82fcb..b2e4ffb 100644
+        --- a/deploy-complexity.gemspec
+        +++ b/deploy-complexity.gemspec
+        @@ -26,10 +26,10 @@ Gem::Specification.new do |spec|
+           spec.add_development_dependency "bundler", "~> 2.1"
+           spec.add_development_dependency "rake", "~> 13.0"
 
-+  spec.add_dependency "git", "~> 1.7.0"
-   spec.add_dependency "octokit", "~> 4.0"
-   spec.add_dependency "slack-notifier", "~> 2.3.2"
-   spec.add_dependency "values", "~> 1.8.0"
--  spec.add_dependency "git", "~> 1.7.0"
+        +  spec.add_dependency "git", "~> 1.7.0"
+           spec.add_dependency "octokit", "~> 4.0"
+           spec.add_dependency "slack-notifier", "~> 2.3.2"
+           spec.add_dependency "values", "~> 1.8.0"
+        -  spec.add_dependency "git", "~> 1.7.0"
 
-   spec.required_ruby_version = ">= 2.5"
- end
-DIFF
+           spec.required_ruby_version = ">= 2.5"
+         end
+      DIFF
 
       spec_add = diff('deploy-complexity.gemspec', patch: patch)
       spec_removal = diff('foo', patch: "\n-  spec.add_dependency\n")
